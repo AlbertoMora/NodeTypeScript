3d12e842c368e444c7a3b98c4c4f6f1bbb0b7b76
Alberto Mora <alberto.mora@dakolum.com>
2022-09-23 02:49:44 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const spawn = require('cross-spawn');
+const fs = require('fs');
+const path = require('path');
+
+const projectName = process.argv[2] || 'template-project';
+
+const currentDir = process.cwd();
+const projectDir = path.resolve(currentDir, projectName);
+
+const allDependencies = ['dotenv', 'cors'];
+const allDevDependencies = ['typescript', 'ts-node', 'ts-loader', 'copyfiles', 'nodemon'];
+
+const install = () => {
+    let command = 'npm';
+    const args = ['install', '--no-audit', '--save', '--save-exact', '--loglevel', 'error'].concat(allDependencies);
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', code => {
+        if (code !== 0) {
+            console.log('Dependecies installation failed. Please retry the installation manually');
+        } else {
+            console.log('Dependencies installed successfully');
+            const devArgs = ['install', '--no-audit', '--dev', '--loglevel', 'error'].concat(allDevDependencies);
+            const child2 = spawn(command, devArgs, { stdio: 'inherit' });
+            child2.on('close', code => {
+                if (code !== 0) {
+                    console.log('Dev dependecies installation failed. Please retry the installation manually');
+                } else {
+                    console.log('Dev dependencies installed successfully');
+                    console.log('====================================');
+                    console.log('Project has been templated successfully');
+                    console.log('====================================');
+                }
+            });
+        }
+    });
+};
+
+const init = () => {
+    fs.mkdirSync(projectDir, { recursive: true });
+
+    const templateDir = path.resolve(__dirname, 'template');
+    fs.cpSync(templateDir, projectDir, { recursive: true });
+
+    fs.renameSync(path.join(projectDir, 'gitignore'), path.join(projectDir, '.gitignore'));
+
+    install();
+};
+
+init();
---
+++ template/README.md
@@ -0,0 +1,6 @@
+# Available Commands:
+
+-   npm run tsw: Copies all the new resources to "dist" dir and runs TypeScript compiler (Should be executed before any
+    start command).
+-   npm run start: Executes the project based on "dist/index.js" file, keeping it tracking with Nodemon
+-   npm run prod: Executes the project based on "dist/index.js"
